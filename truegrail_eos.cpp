287dd97df803dcc6baa625568cb906975b3fe137
chipueatfast <15520604@gm.uit.edu.vn>
2019-12-05 11:43:02 +0700

    decompose upsert into insert and update, add more role check, change user table structure

--- truegrail_eos.cpp
+++ truegrail_eos.cpp
@@ -11,6 +11,12 @@ namespace eosio {
             };
 
             [[eosio::action]]
+            bool checkcreator() {
+                require_auth(get_self());
+                return true;
+            }
+
+            [[eosio::action]]
             void clearusers() {
                 require_auth(get_self());
                 users storage(get_self(), get_self().value);
@@ -40,7 +46,7 @@ namespace eosio {
             }
 
             [[eosio::action]]
-            void insertuser(uint64_t user_id, string user_info_hash, string role) {
+            void insertuser(uint64_t user_id, name eos_name, string user_info_hash, string role) {
                 require_auth(get_self());
                 users storage(get_self(), get_self().value);
                 auto existing = storage.find(user_id);
@@ -49,66 +55,70 @@ namespace eosio {
                     row.id = user_id;
                     row.info_hash = user_info_hash;
                     row.role = role;
+                    row.eos_name = eos_name;
                 });
             }
 
             [[eosio::action]]
             void updateuser(name user, uint64_t user_id, string user_info_hash) {
                 require_auth(user);
-            }
-
-            [[eosio::action]]
-            void upsertuser(uint64_t user_id, string user_info_hash) {
-                require_auth(get_self());
                 users storage(get_self(), get_self().value);
                 auto existing = storage.find(user_id);
-                if (existing == storage.end()) {
-                    storage.emplace(get_self(), [&](auto& row) {
-                        row.id = user_id;
-                        row.info_hash = user_info_hash;
-                    });
-                } else {
-                    storage.modify(existing, get_self(), [&](auto& row) {
-                        row.info_hash = user_info_hash;
-                    });
-                };
+                check(existing != storage.end(), "This user does not exist");
+                check(user == existing->eos_name, "This is not the valid user");
+                storage.modify(existing, get_self(), [&] (auto& row) {
+                    row.info_hash = user_info_hash;
+                });
             }
 
             [[eosio::action]]
-            void issue(uint64_t sneaker_id, string sneaker_info_hash) {
-                require_auth(get_self());
-                ownerships storage(get_self(), get_self().value);
+            bool checkfactory(name factory, uint64_t factory_id) {
+                require_auth(factory);
+                users storage(get_self(), get_self().value);
+                auto existing = storage.find(factory_id);
+                check(existing != storage.end() && existing->role == "factory", "This is not a factory cred");
+                return true;
+            }
+
+            [[eosio::action]]
+            void issue(name factory, uint64_t factory_id, name toclaim, uint64_t sneaker_id, string sneaker_info_hash) {
+                check(checkfactory(factory, factory_id), "You are not the factory");
+                sneakers storage(get_self(), get_self().value);
                 struct sneaker new_sneaker = {
                     .id = sneaker_id,
                     .info_hash = sneaker_info_hash,
+                    .owner_id = 0,
+                    .owner = toclaim,
+                    .status = "new",
                 };
                 storage.emplace(get_self(), [&](auto& row) {
-                    row.sneaker = new_sneaker;
+                    row = new_sneaker;
                 });
             };
 
 
-            [[eosio::action]]
-            void transfer(uint64_t sneaker_id, uint64_t user_id, string user_info_hash) {
-                require_auth(get_self());
-                ownerships storage(get_self(), get_self().value);
-                auto existing = storage.find(sneaker_id);
-                check(existing != storage.end(), "sneaker does not exist!");
-                storage.modify(existing, get_self(), [&] (auto& row) {
-                    row.sneaker = existing -> sneaker;
-                    struct user new_owner = {
-                        .id = user_id,
-                        .info_hash = user_info_hash,
-                    };
-                    row.owner = new_owner;
-                });
-            }
+            // [[eosio::action]]
+            // void transfer(uint64_t sneaker_id, uint64_t user_id, string user_info_hash) {
+            //     require_auth(get_self());
+            //     ownerships storage(get_self(), get_self().value);
+            //     auto existing = storage.find(sneaker_id);
+            //     check(existing != storage.end(), "sneaker does not exist!");
+            //     storage.modify(existing, get_self(), [&] (auto& row) {
+            //         row.sneaker = existing -> sneaker;
+            //         struct user new_owner = {
+            //             .id = user_id,
+            //             .info_hash = user_info_hash,
+            //         };
+            //         row.owner = new_owner;
+            //     });
+            // }
 
 
         private:
 
             struct [[eosio::table]] user {
                 uint64_t id;
+                name eos_name;
                 string info_hash;
                 string role;
 
@@ -117,9 +127,16 @@ namespace eosio {
                 }
             };
 
-            struct sneaker {
+            struct [[eosio::table]] sneaker {
                 uint64_t id;
                 string info_hash;
+                uint64_t owner_id;
+                name owner;
+                string status;
+
+                uint64_t primary_key()const {
+                    return id;
+                }
             };
 
             struct [[eosio::table]] history {
@@ -140,7 +157,7 @@ namespace eosio {
             };
 
             typedef multi_index<"users"_n, user> users;
-            typedef multi_index<"ownerships"_n, ownership> ownerships;
+            typedef multi_index<"sneakers"_n, sneaker> sneakers;
     };
 }
 
